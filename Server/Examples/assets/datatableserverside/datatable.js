ba62cc2e520df3c32d534e3cdc3fb98d4fd5504c
VladimirDimov <vdimov1124@gmail.com>
2016-09-10 00:41:28 +0300

    spinners transparency modified

--- Server/Examples/assets/datatableserverside/datatable.js
+++ Server/Examples/assets/datatableserverside/datatable.js
@@ -966,15 +966,20 @@ var spinner = (function () {
             if (table.settings.spinner.enable === false) {
                 return;
             }
-            
+
             setSpinner(table);
             table.events.onDataLoading.push(renderSpinner);
         }
     };
 
     function setSpinner(table) {
+        var width = 200;
         var spinnerStyle = table.settings.spinner.style;
         var $spinnerRow = $('<tr/>');
+        // $spinnerRow.css('z-index', -5000);
+        $spinnerRow.css('position', 'fixed');
+        $spinnerRow.css('left', '40%');
+        $spinnerRow.css('top', '30%');
 
         var $spinnerCell = $('<td/>');
         $spinnerCell.attr('colSpan', table.store.columnPropertyNames.length);
@@ -984,6 +989,7 @@ var spinner = (function () {
         $image.attr('src', '/assets/datatableserverside/img/spinners/' + spinnerStyle + '.gif');
         $image.css('display', 'block');
         $image.css('margin', 'auto auto');
+        $image.css('width', width + 'px');
 
         $spinnerCell.append($image);
         $spinnerRow.append($spinnerCell);
@@ -992,7 +998,7 @@ var spinner = (function () {
 
     function renderSpinner(table) {
         var $tableBody = table.$table.find('tbody');
-        $tableBody.empty();
+        // $tableBody.empty();
         $tableBody.append(table.settings.$spinner);
     }
 
