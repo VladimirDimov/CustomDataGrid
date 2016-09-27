99f915ee461f9ab0797173c842d9a6580cef1709
VladimirDimov <vladimir.dimov@sergata.net>
2016-09-27 15:19:34 +0300

    RequestParamsManager interface extracted

---
+++ Server/DataTables/ProcessDataProviders/Contracts/IRequestParamsManager.cs
@@ -0,0 +1,14 @@
+﻿namespace DataTables.ProcessDataProviders.Contracts
+{
+    using System.Web.Mvc;
+    using DataTables.Models.Request;
+
+    public interface IRequestParamsManager
+    {
+        /// <summary>
+        /// Returns a model with all passed values from the filter context
+        /// </summary>
+        /// <returns>RequestModel</returns>
+        RequestModel GetRequestModel(ActionExecutedContext filterContext);
+    }
+}
